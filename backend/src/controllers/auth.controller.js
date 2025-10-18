aef1289ae26886e6019e4d3f39e35047052db092
Dhruv Solanki <dhruvsolanki0129@gmail.com>
2025-10-18 16:47:53 +0530

    SignUp Functionality with Verification Code

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,26 +1,57 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import { generateCookieAndToken } from "../utils/generateCookieAndToken.js";
 
 export const signupController = async (req, res) => {
-  const {username, email, password} = req.body;
-
-  try {  
-    // Chck everything presents
-    if(!username || !email || !password){
-      return res.status(400).json({success: false, error: true, message: 'All fields are required'});
+  const { username, email, password } = req.body;
+  try {
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        error: true,
+        message: "All Fields are Required",
+      });
     }
 
-    // Chck if user already exists or not
-    
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ success: false, error: true, message: "User Already Exists" });
+    }
 
-    // Return the Response that user is being created successfully
-    
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const verificationToken = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+
+    const user = new User({
+      username,
+      email,
+      password: hashedPassword,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
+    });
+
+    await user.save();
+    generateCookieAndToken(res, user._id);
+
+    return res.status(201).json({
+      success: true,
+      error: false,
+      message: "User Created Successfully",
+      user: { ...user._doc, password: undefined },
+    });
   } catch (error) {
-    console.log('Error in SignUp: ', error.message);
-    return res.status(500).json({success: false, error: true, message: 'Internal Server Error!!!'});
+    console.log("Error in SignUp Controller: ", error.message);
+    return res.status(500).json({
+      success: false,
+      error: true,
+      message: "Error Signing Up the User",
+    });
   }
-  
 };
 
 export const loginController = async (req, res) => {};
 
-export const logoutController = async (req, res) => {};
+export const logoutController = () => {};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,20 @@
-import express from 'express';
-import 'dotenv/config';
+import cookieParser from "cookie-parser";
+import express from "express";
+import "dotenv/config";
 
 // Routes
-import authRoutes from './routes/auth.routes.js';
+import authRoutes from "./routes/auth.routes.js";
 
 // Database
-import { connectDB } from './database/connectDB.js';
+import { connectDB } from "./database/connectDB.js";
 
 const app = express();
-const PORT = process.env.port || 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is Running on Port: ${PORT}`);
